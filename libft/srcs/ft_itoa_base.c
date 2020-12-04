032f51b6c83585e6e25ece169bf8be76850755ff
AzodFR <azod.contact@gmail.com>
2020-12-04 18:23:33 +0100

    split fc

--- libft/srcs/ft_itoa_base.c
+++ libft/srcs/ft_itoa_base.c
@@ -6,39 +6,50 @@
 /*   By: thjacque <marvin@r42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 18:05:06 by thjacque          #+#    #+#             */
-/*   Updated: 2020/12/04 18:16:02 by thjacque         ###   ########lyon.fr   */
+/*   Updated: 2020/12/04 18:23:02 by thjacque         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*convert_positive(char *res, char *base, int n)
+{
+	int		i;
+	int		base_size;
+
+	i = 0;
+	base_size = ft_strlen(base);
+	while (n)
+	{
+		res[i++] = base[n % base_size];
+		n /= base_size;
+	}
+	return (res);
+}
+
+char	*convert_negative(char *res, char *base, unsigned int n)
+{
+	int		i;
+	int		base_size;
+
+	i = 0;
+	base_size = ft_strlen(base);
+	while (n)
+	{
+		res[i++] = base[n % base_size];
+		n /= base_size;
+	}
+	return (res);
+}
+
 char	*ft_itoa_base(int	n, char *base)
 {
 	char			*res;
-	int				i;
-	int				base_size;
-	unsigned int	u_n;
 
 	if (!(res = malloc(8 * sizeof(char))))
 		return (NULL);
-	base_size = ft_strlen(base);
-	i = 0;
 	if (n > 0)
-	{
-		while (n)
-		{
-			res[i++] = base[n % base_size];
-			n /= base_size;
-		}
-	}
+		return (convert_positive(res, base, n));
 	else
-	{
-		u_n = n;
-		while (u_n)
-		{
-			res[i++] = base[u_n % base_size];
-			u_n /= base_size;
-		}
-	}
-	return (res);
+		return (convert_negative(res, base, n));
 }
